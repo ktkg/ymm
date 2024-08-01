4f2bce1c2ff1c26a234ceb8e613d034b1816bf70
ktkg <kaorutkg@gmail.com>
2024-08-02 01:06:36 +0900

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>都道府県別の総人口推移</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="都道府県別の総人口推移" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>都道府県別の総人口推移</p>
+      </main>
+    </>
+  );
+}
