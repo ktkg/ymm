72e7c521f31a014955a687f8d44a26bcee6d02e4
ktkg <33220505+ktkg@users.noreply.github.com>
2024-08-02 03:32:56 +0900

    chore: linterの導入 (#1)
    * chore: commitlintの設定
    
    * chore: eslintの設定
    
    * chore: stylelintの導入

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import configPrettier from "eslint-config-prettier";
+import globals from "globals";
+import pluginImport from "eslint-plugin-import";
+import pluginJs from "@eslint/js";
+import pluginNext from "@next/eslint-plugin-next";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default [
+  { ignores: [".next/*"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  configPrettier,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+      "@next/next": pluginNext,
+      import: pluginImport,
+    },
+    rules: {
+      ...pluginReact.configs["jsx-runtime"].rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+      ...pluginImport.configs.recommended.rules,
+      "react/self-closing-comp": "error",
+      "import/order": ["error", { "newlines-between": "always" }],
+      "import/newline-after-import": "error",
+    },
+  },
+];
