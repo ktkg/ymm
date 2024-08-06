70a4cc6ca4e1ef2b7e63e1bb26f914c46d633b39
ktkg <kaorutkg@gmail.com>
2024-08-07 03:16:27 +0900

    feat: 都道府県データの取得とトップページの実装

---
+++ src/components/top-page/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import { Prefectures } from "./prefectures";
+
+import { PrefectureModel } from "@/model/prefecture.model";
+
+type Props = {
+  prefectures: PrefectureModel[];
+};
+
+export const TopPage = ({ prefectures }: Props) => {
+  const [selectedPrefCodes, setSelectedPrefCodes] = useState<
+    PrefectureModel["prefCode"][]
+  >([]);
+
+  const handleSelectPrefecture = (target: PrefectureModel["prefCode"]) => {
+    setSelectedPrefCodes((prev) => {
+      if (prev.includes(target)) {
+        return prev.filter((prefCode) => prefCode !== target);
+      } else {
+        return [...prev, target];
+      }
+    });
+  };
+
+  return (
+    <main>
+      <Prefectures
+        prefectures={prefectures}
+        selectedPrefCodes={selectedPrefCodes}
+        onClickCheckbox={handleSelectPrefecture}
+      />
+    </main>
+  );
+};
