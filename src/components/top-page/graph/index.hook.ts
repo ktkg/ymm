f69553e8de431c6fe9d1208a721ec4d374f878dd
ktkg <kaorutkg@gmail.com>
2024-08-08 04:10:02 +0900

    feat: 人口推移情報を取得してパースするカスタムフックの実装

---
+++ src/components/top-page/graph/index.hook.ts
@@ -0,0 +1,26 @@
+import useSWR from "swr";
+import { z } from "zod";
+
+import { populationResponseSchema } from "@/model/population.model";
+import { PrefectureModel } from "@/model/prefecture.model";
+
+export const usePopulation = (prefCodes: PrefectureModel["prefCode"][]) => {
+  const swrResponse = useSWR<
+    z.infer<typeof populationResponseSchema>[],
+    Error,
+    string[]
+  >(
+    prefCodes.map((prefCode) => `/api/population/${prefCode}`),
+    async (urls) => {
+      const responses = await Promise.all(
+        urls.map((url) => fetch(url).then((res) => res.json())),
+      );
+
+      return responses.map((response) =>
+        populationResponseSchema.parse(response),
+      );
+    },
+  );
+
+  return swrResponse;
+};
---
+++ src/model/population.model.ts
@@ -0,0 +1,74 @@
+import { z } from "zod";
+
+import { PrefectureModel } from "./prefecture.model";
+
+export const populationType = [
+  {
+    id: "total",
+    label: "総人口",
+  },
+  {
+    id: "young",
+    label: "年少人口",
+  },
+  {
+    id: "productive",
+    label: "生産年齢人口",
+  },
+  {
+    id: "elderly",
+    label: "老年人口",
+  },
+] as const;
+
+export type PopulationType = (typeof populationType)[number]["id"];
+
+export const populationResponseSchema = z.object({
+  data: z.object({
+    message: z.nullable(z.string()),
+    result: z.object({
+      boundaryYear: z.number(),
+      data: z.array(
+        z.object({
+          label: z.string(),
+          data: z.array(
+            z.object({
+              year: z.number(),
+              value: z.number(),
+            }),
+          ),
+        }),
+      ),
+    }),
+  }),
+  prefCode: z.number(),
+});
+
+type PopulationModel = {
+  prefCode: PrefectureModel["prefCode"];
+  boundaryYear: number;
+  data: {
+    [key in PopulationType]: {
+      year: number;
+      value: number;
+    }[];
+  };
+};
+
+export const createPopulationList = (
+  parsedResponses: z.infer<typeof populationResponseSchema>[],
+): PopulationModel[] =>
+  parsedResponses.map((parsedResponse) => {
+    const { boundaryYear, data } = parsedResponse.data.result;
+
+    return {
+      prefCode: parsedResponse.prefCode,
+      boundaryYear,
+      data: Object.fromEntries(
+        populationType.map(({ id, label }) => [
+          id,
+          data.find((d) => d.label === label)!.data,
+        ]),
+      ) as PopulationModel["data"],
+    };
+  });
